f783cb21fdbdf13598afb9ef1432108d1b8006a5
Samantha Gundotra <40836349+samgundotra@users.noreply.github.com>
2022-09-11 17:42:05 -0400

    fruit filtering

--- app.js
+++ app.js
@@ -26,12 +26,25 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
+  let highSugar = fruitsArray.filter(function (e) {
+    return e.nutritions.sugar > 15;
+});
+  console.log(highSugar);
 
   // TODO: Create a new HTML element to display your data
+  const newElement = document.createElement('div');
+  const existingElement = document.getElementById('cs1300-gallery');
+
+  let highSugarFruits = highSugar.map(function (e) {
+    const newElement = document.createElement('div');
+    newElement.innerHTML = e.name;
+    existingElement.append(newElement);
+  } );
+  
 
   // TODO: Append your new element to the page
 
